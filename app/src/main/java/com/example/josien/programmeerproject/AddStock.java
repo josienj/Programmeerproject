8d22d194fb0ac45aec2c27b71616fb206e20511e
josienj <josienjansen1@gmail.com>
2016-06-02 00:30:11 +0200

    opzet xml

---
+++ app/src/main/java/com/example/josien/programmeerproject/AddStock.java
@@ -0,0 +1,100 @@
+package com.example.josien.programmeerproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * Created by Josien on 1-6-2016.
+ */
+public class AddStock extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addstock);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_bierlijst) {
+            Intent bierlijst = new Intent(this, MainActivity.class);
+            bierlijst.putExtra("Bierlijst", 500);
+            startActivity(bierlijst);
+        } else if (id == R.id.nav_addvoorraad) {
+            Toast.makeText(this, "Je bent al op deze pagina", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_checkvoorraad) {
+            Intent checkvoorraad = new Intent(this, CheckStock.class);
+            checkvoorraad.putExtra("check_voorraad", 500);
+            startActivity(checkvoorraad);
+        } else if (id == R.id.nav_informatie) {
+            Intent information = new Intent(this, Information.class);
+            information.putExtra("information", 500);
+            startActivity(information);
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/josien/programmeerproject/MakingGroup.java
@@ -0,0 +1,7 @@
+package com.example.josien.programmeerproject;
+
+/**
+ * Created by Josien on 1-6-2016.
+ */
+public class MakingGroup {
+}
